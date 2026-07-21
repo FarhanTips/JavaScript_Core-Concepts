c5414d8dc3071f14f75745a4b8a1639ba604d70c
FarhanTips <farhanakbor02@gmail.com>
2026-07-22 02:34:10 +0600

    Simple Conditionals

---
+++ 2. Fundamental of Conditionals/2. Simple and Multiple Conditions.js
@@ -0,0 +1,22 @@
+
+// Practise question 7
+const speed = 65;
+if (speed > 80) {
+    console.log("Overspeeding");
+} else {
+    console.log("Normal Speed");
+}
+
+
+
+// Practise question 8
+const age = 65;
+const ticket = false;
+
+if (age > 18 && ticket) {
+    console.log("Entry allowed");
+} else {
+    console.log("Not allowed");
+}
+
+
