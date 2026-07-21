0c10e174bcf0a8ec860198349ecb322943ba36ba
FarhanTips <farhanakbor02@gmail.com>
2026-07-21 22:10:24 +0600

    Shorthand

--- 1. Introduction to JavaScript/4. Mathematical Operation and Shorthand.js
+++ 1. Introduction to JavaScript/4. Mathematical Operation and Shorthand.js
@@ -4,4 +4,46 @@ let c = a+b;
 console.log(c);
 console.log(typeof c);
 
-kl
+
+//Addition
+let one = 20;
+let two = 30;
+let total = one + two;
+console.log(total);
+
+//subtraction
+total = one - two;
+console.log(total);
+
+//Multiplication
+total = one * two;
+console.log(total);
+
+//Division
+total = one / two;
+console.log(total);
+
+
+// % হলো modulo (বা remainder) operator
+total = one % two;
+console.log(total);
+
+
+// Increment and Decrement
+let number = 7;
+number++;
+number++;
+console.log(number);
+number--;
+console.log(number);
+
+
+// Add & assign
+let weight = 76;
+weight += 5;
+console.log(weight)
+
+// Multiply & assign
+let marks = 12;
+marks *= 3;
+console.log(marks);
