7039a401fd9350492f75e94d857ef7a2ff9b549e
FarhanTips <farhanakbor02@gmail.com>
2026-07-22 02:15:32 +0600

    Comparison Operators

---
+++ 2. Fundamental of Conditionals/1. Comparison Operators.js
@@ -0,0 +1,18 @@
+const price1 = 45;
+const price2 = 55;
+
+// console.log(price1 > price2);
+// console.log(price1 < price2);
+// console.log(price1 === price2);
+// console.log(price1 >= price2);
+// console.log(price1 <= price2);
+// console.log(price1 !== price2);
+
+
+// Difference betwee === and ==
+const weight = 78;
+const address = "78";
+console.log(weight === address);
+console.log(weight == address);
+// If confuse then ask AI
+
