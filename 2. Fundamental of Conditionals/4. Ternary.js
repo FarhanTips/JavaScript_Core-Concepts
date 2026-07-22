5b9d67ff09dde3b61e390e440420f1c4ff1ac242
FarhanTips <farhanakbor02@gmail.com>
2026-07-22 21:47:01 +0600

    Ternary Operator

---
+++ 2. Fundamental of Conditionals/4. Ternary.js
@@ -0,0 +1,23 @@
+
+// condition ? expression1 : expression2;
+
+// Practise question 16
+const number = 7;
+
+number % 2 === 0 ? console.log("Even") : console.log("Odd");
+
+
+
+// const variable = condition ? valueIfTrue : valueIfFalse;
+
+const price = 6000;
+
+const finalPrice = price>5000 ? (price - price*0.3) : price;
+console.log("Final Price:",finalPrice+ " tk");
+
+
+
+console.log(condition ? valueIfTrue : valueIfFalse);
+// Practise question 18
+const money = 100;
+console.log(money>1000 ? "Expensive" : "Affordable");
