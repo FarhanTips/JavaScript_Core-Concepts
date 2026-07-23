041fbfa94f3a6c1aadf50836288831b1966944c3
FarhanTips <farhanakbor02@gmail.com>
2026-07-23 18:15:27 +0600

    Simple Problems using loops

---
+++ 3. Fundamental Concepts of Loops/1. Simple Examples.js
@@ -0,0 +1,35 @@
+// Practise Task-3
+let i = 10;
+while (i>0) {
+    console.log(i);
+    i--;
+}
+
+
+
+
+// Practise Task-4
+let x = 0;
+while (x<7) {
+    console.log("Loop চলছে",x+1);
+    x++;
+}
+
+
+
+
+// Practise Task-5
+for (let y = 1; y<31; y++) {
+    if (y%2 === 0) {
+        console.log(y, "is an Even Number.");
+    }
+}
+
+
+
+// Practise Task-6
+let sum = 0;
+for (let w = 1; w<=20; w++) {
+    sum += w;
+}
+console.log("Sum from 1 to 20:",sum);
