1d3edd7efba9ef593455d69fc04b73d2ae568469
FarhanTips <farhanakbor02@gmail.com>
2026-07-24 11:27:18 +0600

    Difference of loops

---
+++ 3. Fundamental Concepts of Loops/3. Comparison of Different Loops.js
@@ -0,0 +1,44 @@
+
+// Practise Task-13
+
+// for loop
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+// while loop
+
+let x = 1;
+
+while (x <= 5) {
+    console.log(x);
+    x++;
+}
+
+
+
+// do while loop
+
+let y = 1;
+
+do {
+    console.log(y);
+    y++;
+} while (y <= 5);
+
+
+
+/**
+ * 
+ * 
+ * Which one should you use?
+ * 
+for → Best when you know how many times the loop should run (like printing 1 to 5).
+
+while → Best when you don't know in advance how many times you'll loop; you just keep going until a condition becomes false.
+
+do...while → Best when the code must run at least once, even if the condition is false initially.
+
+ */
