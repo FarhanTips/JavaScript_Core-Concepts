69b44c03bb9cc7576379713f2888da4e6725d186
FarhanTips <farhanakbor02@gmail.com>
2026-07-24 23:06:02 +0600

    Intro to Array

---
+++ 4. Fundamental Concepts of Array/1. Beginner Problems Array.js
@@ -0,0 +1,44 @@
+
+
+// Practise Task-3
+const numbers = [21, 65, 12, 57, 77, 99, 81];
+console.log(numbers[0]);
+console.log(numbers[2]);
+
+// Practise Task-4
+numbers[1] = 1000;
+console.log(numbers);
+
+
+// Practise Task-5
+numbers.push(5111);
+console.log(numbers);
+
+// Practise Task-6
+numbers.pop();
+console.log(numbers);
+
+
+
+// Practise Task-7
+numbers.unshift(999999);
+console.log(numbers);
+
+numbers.unshift(-21, -32, -5);
+console.log(numbers);
+
+
+
+
+// Practise Task-8
+const shiftedValue = numbers.shift();
+console.log(numbers);
+console.log(shiftedValue);
+
+
+
+// Practise Task-9
+console.log(numbers.includes(12));
+
+
+console.log(numbers.indexOf(12));
