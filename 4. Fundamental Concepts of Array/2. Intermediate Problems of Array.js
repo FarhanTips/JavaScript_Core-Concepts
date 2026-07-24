270a53ba284675dfe6a3e6e6792bcb004da7129d
FarhanTips <farhanakbor02@gmail.com>
2026-07-25 00:01:26 +0600

    Intermediate problems of Array

---
+++ 4. Fundamental Concepts of Array/2. Intermediate Problems of Array.js
@@ -0,0 +1,42 @@
+
+// Practise Task-1
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+let arr3 = [5, 6];
+
+let newArray = arr1.concat(arr2, arr3);
+
+console.log(newArray);
+
+
+
+// Practise Task-2
+const numbers = [21, 65, 12, 57, 77, 99, 81];
+const portion = numbers.slice(2, 6);
+console.log(portion);
+
+
+// Practise Task-3
+const words = ["I", "love", "my", "country"];
+const sentence = words.join(",");
+console.log(sentence);
+
+
+
+// Practise Task-4
+console.log(Array.isArray(words));
+
+
+
+// Practise Task-5
+const text = ["babul", "kabul", "Mokbul", "Dadul", "Fijul"];
+const reversed = text.reverse();
+console.log(reversed);
+
+
+
+
+// Practise Task-6
+const names = ["babul", "kabul", "Mokbul", "Dadul", "Fijul"];
+const sortedText = names.sort();
+console.log(sortedText);
