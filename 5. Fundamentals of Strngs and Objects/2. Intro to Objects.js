f0e8a363a46de4b58fe22f046055c28e36940a73
FarhanTips <farhanakbor02@gmail.com>
2026-07-27 19:52:03 +0600

    Intro to Objects

---
+++ 5. Fundamentals of Strngs and Objects/2. Intro to Objects.js
@@ -0,0 +1,19 @@
+
+
+const sunglass = {
+    color: "White",
+    price: 550,
+    brand: "Raymond"
+};
+console.log(sunglass);
+
+
+const phone = {
+    brand: "Samsung",
+    version: "A55",
+    isThreeCamera: true,
+    secondHand: false,
+    appsInstalled: ["FB", "Youtube", "ChatGpt"]
+};
+
+console.log(phone);
