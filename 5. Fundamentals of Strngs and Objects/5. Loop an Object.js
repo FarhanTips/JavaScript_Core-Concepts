35e0fac0837106446a64ee870576080dd42ad766
FarhanTips <farhanakbor02@gmail.com>
2026-07-28 02:08:12 +0600

    Array traversal by for loop

---
+++ 5. Fundamentals of Strngs and Objects/5. Loop an Object.js
@@ -0,0 +1,17 @@
+
+// array traversal by loop (for-of)
+const numbers = [11,22,33,44,55,66];
+for (const number of numbers) {
+    console.log(number);
+}
+
+
+
+
+const person = {
+    name: "Farhan",
+    age: 25,
+    degree: "BSc",
+    "home address": "Purana Paltan Line, Dhaka",
+    "father:name": "Sazzad"
+};
