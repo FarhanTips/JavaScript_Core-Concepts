b3e4857945de4194661415dca58ec29945b321dd
FarhanTips <farhanakbor02@gmail.com>
2026-07-28 18:44:30 +0600

    Intro to Functions

---
+++ 6. Fundamentals of Functions/1. Intro to Functions.js
@@ -0,0 +1,21 @@
+
+
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        return "even";
+    }
+    else {
+        return "odd";
+    }
+}
+
+
+const result = checkEvenOdd(27);
+console.log("27 is an "+result+" number.");
+
+
+console.log("46 is an "+checkEvenOdd(46)+" number.");
+
+
+
+
