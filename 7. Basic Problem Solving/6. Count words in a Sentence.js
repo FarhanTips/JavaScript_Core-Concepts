02414b6ce66ef3f658d2adf8d4c09f29d86d1c05
FarhanTips <farhanakbor02@gmail.com>
2026-07-29 17:40:08 +0600

    Count words in a sentence

---
+++ 7. Basic Problem Solving/6. Count words in a Sentence.js
@@ -0,0 +1,10 @@
+
+
+function countWords(string){
+    string = string.split(" ");
+    return string.length;
+}
+
+console.log(countWords("I love myself"));
+console.log(countWords("I am the king"));
+console.log(countWords("As you can see I am not dead."));
