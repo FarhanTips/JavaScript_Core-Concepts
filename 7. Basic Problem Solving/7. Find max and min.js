b6d18686cf4d71b79d177d00abf96089b49a6cfa
FarhanTips <farhanakbor02@gmail.com>
2026-07-29 18:03:20 +0600

    Find max and min

--- 7. Basic Problem Solving/7. Find max and min.js
+++ 7. Basic Problem Solving/7. Find max and min.js
@@ -14,7 +14,7 @@ function findMaxMin(array){
         }
     }
 
-    return [max, min];  //এখানে [max, min] নতুন array তৈরি করছে, কিন্তু তুমি সেটাকে কোনো variable-এ store করছো না। তাই let, const এর দরকার নাই।
+    return [max, min];  //এখানে [max, min] নতুন array তৈরি করছে, কিন্তু তুমি সেটাকে কোনো variable-এ store করছো না। তাই let, const এর দরকার নাই। এখানে array বানিয়ে সরাসরি return করা হচ্ছে, তাই const/let লাগছে না। নতুন কোনো জিনিসকে নাম দিয়ে রাখতে চাইলে → let/const use করতে হয়। শুধু তৈরি করে সরাসরি use/return করলে → let/const লাগে না।
 }
 
 console.log(findMaxMin([12, 6, 7, 1, 65, 2, 98, 22]));
