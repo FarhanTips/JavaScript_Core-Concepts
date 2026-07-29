2ca54ed780291e7cb80467560eda193e2c9480da
FarhanTips <farhanakbor02@gmail.com>
2026-07-29 17:23:02 +0600

    Palindrome

---
+++ 7. Basic Problem Solving/5. Palindrome check.js
@@ -0,0 +1,23 @@
+
+
+function checkPalindrome(string) {
+    let string2 = ""
+    for (let i = string.length - 1; i >= 0; i--) {
+        // console.log(string[i]);
+        string2 += string[i];
+    }
+    if (string === string2){
+        return "Palindrome";
+    }
+    else {
+        return "Not Palindrome";
+    }
+
+
+}
+
+console.log(checkPalindrome("programming"));
+console.log(checkPalindrome("I love Bangladesh"));
+
+console.log(checkPalindrome("madam"));
+console.log(checkPalindrome("civic"));
